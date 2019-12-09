72ae5c009f4e112d53e9c2c2bd02f7b3f2f86460
Bartłomiej Mroczek <mroczek.bartlomiej@gmail.com>
2019-12-09 17:18:55 +0100

    Review 1.

--- src/Pesel.java
+++ src/Pesel.java
@@ -16,20 +16,21 @@ public class Pesel {
         do {
             System.out.println("Podaj pesel składający się z 11 liczb :");
             pesel = scanner.next();
-            if (pesel.length() > 11 || pesel.length() < 11) {
+            if (pesel.length() > 11 || pesel.length() < 11) { //TODO: ten warunk można zapisać łądniej. Może coś w stylu różny od? 
                 System.out.println("Podałeś niepoprawną ilość znaków.");
             }
 
             for (int i = 0; i < pesel.length(); i++) {
-                sumaKontrolna += 1;
+                sumaKontrolna += 1; //TODO: Sprawdzałeś jak wgląda walidacja numery PESEL? https://pl.wikipedia.org/wiki/PESEL#Cyfra_kontrolna_i_sprawdzanie_poprawno%C5%9Bci_numeru
+             //ogólnie jest sporo gotowego kodu w Javie który to robi. Ale ważne żebyś wiedział co on robi jak tu wkleisz. 
             }
             System.out.println("Suma kontrolna : " + sumaKontrolna);
             sumaKontrolna = 0;
-        } while (pesel.length() != 11);
+        } while (pesel.length() != 11); 
 
-        System.out.println("Data urodzenia to : " + pesel.substring(0, 6));
+        System.out.println("Data urodzenia to : " + pesel.substring(0, 6));// Date bym chciałe jakoś sformatowaną. LocalData, Calendar - mówi Ci to coś?
 
-        int plec = Integer.parseInt(pesel.substring(9, 10));
+        int plec = Integer.parseInt(pesel.substring(9, 10)); //A gdyby zrobić sobie metodę która przyjmie pesel i wypisze odrazu jaka płeć? 
 
         if (plec % 2 == 0) {
             System.out.println("Podany pesel należy to kobiety.");
